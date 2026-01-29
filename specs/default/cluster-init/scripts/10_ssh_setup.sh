0036138b90a4deaad106fb5cdc1aa195577290df
Xavier Pillons (MSFT) <xpillons@microsoft.com>
2026-01-29 12:49:12 +0100

    fix configure ssh keys

--- specs/default/cluster-init/scripts/10_ssh_setup.sh
+++ specs/default/cluster-init/scripts/10_ssh_setup.sh
@@ -10,9 +10,11 @@ source "$CYCLECLOUD_SPEC_PATH/files/common.sh"
 
 function configure_ssh_keys() {
     cp -f "$CYCLECLOUD_SPEC_PATH"/files/init_sshkeys.sh /etc/profile.d # Copy setup script file
+    chmod 644 /etc/profile.d/init_sshkeys.sh
 }
 
-# Configure SSH keys only on login nodes or scheduler node
-if ! is_compute ; then
+# Configure SSH keys on all nodes. If users first connect to login node, scheduler or Open OnDemand node, 
+# there will be no concurrent access issues.
+#if ! is_compute ; then
     configure_ssh_keys
-fi
+#fi
